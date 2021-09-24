d33819292b06b67d5bfcc41280239759f92fa90d
Muhammad Azeem <61928905+Azeemaj101@users.noreply.github.com>
2021-09-25 02:13:50 +0500

    59.0

--- src/CardCer.jsx
+++ src/CardCer.jsx
@@ -13,7 +13,7 @@ const CardCer = (props) => {
     }}
     return (
         <div className="card card_css me-3 mb-3">
-        <img src={process.env.PUBLIC_URL + props.img} className="card-img-top proImg" alt="..."/>
+        <img src={process.env.PUBLIC_URL + props.img} className="card-img-top proImg1" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">{props.name}</h5>
             {funt(props.link)}
