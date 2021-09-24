5cf3e2411ee9b9489401518914318ba379d96f74
Muhammad Azeem <61928905+Azeemaj101@users.noreply.github.com>
2021-09-24 23:07:51 +0500

    2.0

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -7,14 +7,14 @@ const NavBar = () => {
     return (
 <nav className="navbar navbar-expand-lg navbar-dark">
   <div className="container-fluid">
-  <NavLink exact className="navbar-brand" to="/"><img className="top_img" src={logo} alt="..." /></NavLink>
+  <NavLink exact className="navbar-brand" to="/2020_Portfolio_Update_into_React/"><img className="top_img" src={logo} alt="..." /></NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <NavLink exact className="nav-link" activeClassName="text-light" to="2020_Portfolio_Update_into_React/">Home</NavLink>
+          <NavLink exact className="nav-link" activeClassName="text-light" to="/2020_Portfolio_Update_into_React/">Home</NavLink>
         </li>
         <li className="nav-item">
           <NavLink exact className="nav-link" activeClassName="text-light" to="/2020_Portfolio_Update_into_React/certificate">Certificate</NavLink>
